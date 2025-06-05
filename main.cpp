b1339015f123a4bfddeee37491923be82eb1e5f3
sbn317783 <000100925@uca.edu.sv>
2025-06-05 14:14:34 -0600

    En este commit ya estan creadas alguanos place holders para las clases. Perosna B se encargara de agregar a alguanas clases ya declaradas

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ class Evidencia {
     string pregunta3; 
     string pregunta4; 
     string pregunta5;  
-public: 
+    public: 
     Evidencia(string nombre, string contenidoEvidencia, int puntosMorales, int puntosPosiblesCaso, string pregunta1, string pregunta2, string pregunta3, string pregunta4, string pregunta5){
        this -> nombre = nombre;
        this -> contenidoEvidencia = contenidoEvidencia;
@@ -24,17 +24,51 @@ public:
         pregunta3 = pregunta3;
         pregunta4 = pregunta4;
         pregunta5 = pregunta5;
-
     }
+};
+
+class Jugador {
+};
+
+class Humanos {
 
 };
 
+class Personajes {
+public: 
+    string nombre; 
+    string ocupacion; 
+    string relacionConVictor; 
+    int edad; 
+    string testimonio; 
+
+};
+
+class Menu{
+    void mostarMenuPrincipal();
+    void seleccionarCargo();
+};
+
+//para esta clase, apreder el manejo de archivos .txt para poder cargar la evidencia desde afuera
+class archivoManager{
+    /*vector<Evidencia> cargarEvidencias (string ruta)
+    vector<Personaje> cargarTestimonios (string ruta)
+    void mostrarArchivo (string Ruta) // para mostrar una carta o ecena */
+};
+
+
+
+
+
+
 int main()
 {
-    Evidencia C1E1 ("Testimoino de la creatura", "esta evidenia es en la que hablamos con la creatura", 1, 4, "Porque crees que los humanos podriamos hacer esto?", "Es que alcazo tenemos que jugar a ser Dios ", "porque entonces se volvio violenta? ", "Porque no deberiamos meterlo a la carcel? ", "Pregunta5" );
+
+    
+    Evidencia C1E1 ("\nTestimoino de la creatura", "esta evidenia es en la que hablamos con la creatura", 1, 4, "Porque crees que los humanos podriamos hacer esto?", "Es que alcazo tenemos que jugar a ser Dios ", "porque entonces se volvio violenta? ", "Porque no deberiamos meterlo a la carcel? ", "Pregunta5" );
     cout <<  C1E1.nombre<< endl; 
     cout <<  C1E1.contenidoEvidencia <<endl; 
-
+    cout << "-------------------------------------------\n";
     Evidencia C1E2 ( "Testimonio de Victor", "Apelaria al pueblo, pero que tanta credibilida tiene su palabra? ", 1, 2, "Pregunta 1 ", "Pregunta 2","Pregunta 3", "Pregunta 4", "Pregunta 5"  );
     cout << "prueba " << C1E2.nombre <<endl;
     cout << C1E2.contenidoEvidencia <<endl;
