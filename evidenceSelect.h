433ca7c0b41665ec402af2acce5ee7957b6490e1
Carlos Martinez <00171125@uca.edu.sv>
2025-07-01 22:38:05 -0600

    funciones de menú y seleccionar evidencias

---
+++ evidenceSelect.h
@@ -0,0 +1,17 @@
+#ifndef evidence_select_h
+#define evidence_select_h
+
+#include <iostream>
+#include <vector>
+#include "evidencia.h" // header evidencia
+
+using namespace std;
+
+void selectEvidence(vector<Evidencia>& seleccionadas, int& totalPoints);
+
+#endif
+
+
+
+
+
